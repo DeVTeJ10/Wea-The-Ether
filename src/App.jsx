e259f975f1b7df56815a1a4bf2ee1bef82fb92ee
Oru <tejirioru@gmail.com>
2024-09-30 16:43:35 +0100

    putting back the images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import HomePage from "./pages/home/homePage"
-import AboutUsPage from "./pages/aboutUs/aboutUsPage"
-import PropertiesPage from "./pages/properties/propertiesPage"
-import ServicesPage from "./pages/services/servicesPage"
-import ForgotPasswordPage from "./pages/forgotPassword/forgotPassword"
-import ResetPasswordPage from "./pages/resetPassword/resetPassword"
-import CreatePost from "./pages/createPost/createPost"
-import SignupPage from "./Auth/Signup/signup"
-import LoginPage from "./Auth/Login/login"
+import HomePage from "./Pages/homepage/homePage"
+
 
 
 
@@ -20,14 +13,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/about-us" element={<AboutUsPage/>} />
-        <Route path="/properties-page/:id" element={<PropertiesPage/>} />
-        <Route path="/services-page" element={<ServicesPage/>} />
-        <Route path="/signup" element={<SignupPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage/>} />
-        <Route path="/reset-password/:token" element={<ResetPasswordPage/>} />
-        <Route path="/create-post" element={<CreatePost/>} />
       </Routes>
     </>
   )
